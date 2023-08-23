3c5c4d4b3a7c1cd3b77b040a8bf506e8c885a07c
PranotiGunjal <142731775+PranotiGunjal@users.noreply.github.com>
2023-08-23 14:34:52 +0530

    Add files via upload

---
+++ WordStatistics.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class WordStatistics {
+    public static void main(String[] args) {
+        String[] words = {"apple", "banana", "apple", "orange", "banana", "apple"};
+
+        HashSet<String> stopWords = new HashSet<>();
+        stopWords.add("the");
+        stopWords.add("and");
+        stopWords.add("is");
+        // Add more stop words as needed
+
+        HashMap<String, Integer> wordCounts = new HashMap<>();
+
+        for (String word : words) {
+            if (!stopWords.contains(word)) {
+                if (wordCounts.containsKey(word)) {
+                    wordCounts.put(word, wordCounts.get(word) + 1);
+                } else {
+                    wordCounts.put(word, 1);
+                }
+            }
+        }
+
+        int totalCount = 0;
+        for (int count : wordCounts.values()) {
+            totalCount += count;
+        }
+
+        int uniqueWordCount = wordCounts.size();
+
+        System.out.println("Word counts (excluding stop words):");
+        for (String word : wordCounts.keySet()) {
+            System.out.println(word + ": " + wordCounts.get(word));
+        }
+
+        System.out.println("Total word count (excluding stop words): " + totalCount);
+        System.out.println("Number of unique words: " + uniqueWordCount);
+    }
+}
